919629240efde2085b209b708595e71b6219efc1
Berke Dönmez <brkdnmz99@gmail.com>
2025-12-08 14:28:04 +0300

    add 2025 day 8

---
+++ 2025/day8/2.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+// the parts weren't so different
+
+int main() {
+    freopen("1.txt", "r", stdin);
+    string s;
+    vector<array<ll, 3>> boxes;
+    while (getline(cin, s)) {
+        regex rgx("(\\d*),(\\d*),(\\d*)");
+        smatch matches;
+        regex_search(s, matches, rgx);
+        ll x = stoll(matches[1].str()), y = stoll(matches[2].str()), z = stoll(matches[3].str());
+        boxes.push_back({x, y, z});
+    }
+    int n = boxes.size();
+    vector<int> p(n), sz(n);
+    for (int i = 0; i < n; i++)
+        p[i] = i, sz[i] = 1;
+    auto f = [&](auto &&self, int x) -> int { return p[x] = (x == p[x] ? x : self(self, p[x])); };
+    auto c = [&](int x, int y) {
+        x = f(f, x), y = f(f, y);
+        if (x == y)
+            return 0;
+        p[y] = x, sz[x] += sz[y];
+        return 1;
+    };
+    auto sq = [](ll x) { return x * x; };
+    vector<array<ll, 3>> edges;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            edges.push_back(
+                {sq(boxes[i][0] - boxes[j][0]) + sq(boxes[i][1] - boxes[j][1]) + sq(boxes[i][2] - boxes[j][2]), i, j});
+        }
+    }
+    sort(edges.begin(), edges.end());
+    ll ans = 0;
+    for (int i = 0; i < edges.size(); i++) {
+        if (c(edges[i][1], edges[i][2]))
+            ans = boxes[edges[i][1]][0] * boxes[edges[i][2]][0];
+    }
+
+    cout << ans << "\n";
+}
